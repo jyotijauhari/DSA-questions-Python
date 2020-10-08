f0b9e4bff213a5f3dc7bdd11339718505c80a5c4
Jyoti Jauhari <jyotijauhari222@gmail.com>
2020-10-08 12:58:52 +0530

    Create group_anagrapm.py

---
+++ group_anagrapm.py
@@ -0,0 +1,13 @@
+#adding values in hashtable aka dict (dict_name[key]=value)
+#input: [go,act,flop,tac, cat,og,lofp]
+#output: [[go,og],[act,cat,tac][flop,lofp]]
+
+def groupAnagram(words):
+	anagram = {}
+	for word in words:
+	sortedword = "".join(sorted(word) #creating sortedword key as string from list which is sorted (eg. bca->abc)
+		if sortedword in anagram:
+			anagram[sortedword].append(word) #for key sortedword i.e abc add bca in list
+		else:
+			anagram[sortedword] = [word]  #create key value pair initially for word tht doesnt exist in hashtable
+	return list(anagram.values()) #returning all values i.e list of all keys
