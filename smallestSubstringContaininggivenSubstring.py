9e4158248387a6a58a51f5c77edd2987f6bbfbfb
Jyoti Jauhari <jyotijauhari222@gmail.com>
2020-10-10 17:48:09 +0530

    Create smallestSubstringContaininggivenSubstring.py

---
+++ smallestSubstringContaininggivenSubstring.py
@@ -0,0 +1,58 @@
+
+#O(b+s) time | O(b+s) space
+def get smallestSubstringContaining(bigString, smallString):
+	targetCharCounts = getCharCounts(smallstring)
+	substringBounds = getSubstringBounds(bigString, targetCharCounts)
+	return getStringFromBounds(bigString, substringBounds )
+
+def getCharCounts(string):
+	charCounts = {}
+	for char in string:
+		increaseCharCount(char, charCounts)
+	return charCounts
+
+def getSubstringBounds(string, targetCharCounts):
+	substringBounds = [0, float("inf")]
+	substringCharCounts = {}
+	numUniqueChars = len(targetCharCounts.keys())
+	numUniqueCharsDone = 0
+	leftIdx = 0
+	rightIdx = 0
+	while rightIdx < len(string):
+		rightChar = string[rightIdx]
+		if rightChar not in targetCharCounts :
+			rightIdx += 1
+			continue
+		increaseCharCount(rightChar, substringCharCounts)
+		if substringCharCounts[rightChar] == targetCharCounts[rightChar]
+			numUniqueCharCountsDone += 1
+		while numUniqueCharsDone == numUniqueChars and leftIdx <= rightIdx :
+			substringBounds = getCloserBounds(leftIdx, rightIdx, substringBounds[0], substringBounds[1])
+			leftChar = string[leftIdx]
+			if leftChar not in targetCharCounts:
+				leftIdx += 1
+				continue
+			if substringCharCounts[leftChar] == targetCharCounts[leftChar]:
+				numUniqueCharsDone -= 1
+			decreaseCharCounts(leftChar, substringCharCounts)
+			leftIdx += 1
+		rightIdx += 1
+	return substringBounds
+
+
+def getCloserBounds(idx1, idx2, idx3, idx4):
+	return [idx1,idx2] if idx2 - idx1 < idx4 - idx3 else [idx3, idx4]
+
+getStringFromBounds(string, bounds):
+	start, end = bounds
+	if end == float("inf"):
+		return ""
+	return string[start : end+1 ]
+
+def increaseCharCount(char, charCounts):
+	if char not in charCounts:
+		charCounts[char] = 0
+	charCounts[char] += 1
+
+def decreaseCharCount(char, charCounts):
+	charCounts[char] -= 1
