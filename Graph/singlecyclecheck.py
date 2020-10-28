8973efbe705c0e79800f4ad623f7e9b9ad51defd
Jyoti Jauhari <jyotijauhari222@gmail.com>
2020-10-28 17:09:05 +0530

    Update singlecyclecheck.py

--- Graph/singlecyclecheck.py
+++ Graph/singlecyclecheck.py
@@ -11,7 +11,7 @@ def hasSingleCycle(array):
 		currentIdx = getNextIdx(currentIdx, array)
 	return currentIdx == 0
 
-def getNexyYdx(currentIdx, array):
+def getNextIdx(currentIdx, array):
 	jump = array[currentIdx]
 	nextIdx = (currentIdx + jump) % len(array)
 	return nextIdx if nextIdx >= 0 else nextIdx + len(array)
