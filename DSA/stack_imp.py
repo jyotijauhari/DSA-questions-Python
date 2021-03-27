23b841a1958c00d9a2bea42dd5e5011944654128
Jyoti Jauhari <jyotijauhari222@gmail.com>
2021-03-27 19:36:51 +0530

    Create stack_imp.py

---
+++ DSA/stack_imp.py
@@ -0,0 +1,39 @@
+class Stack:
+  def __init__(self):
+    self.s = []
+    
+
+  def push(self, x):
+    self.s.append(x) 
+
+  def pop(self):
+    self.s.pop()  
+
+  def top(self):
+    return self.s[-1]  
+
+  def prints(self):
+    for i in self.s:
+      print(i, end = " ")
+    print()  
+    
+s1 = Stack()     
+s1.push(5)
+s1.push(6)
+s1.push(7)
+s1.push(8)
+s1.push(9)
+s1.prints()
+
+s1.pop()
+s1.prints()
+
+s1.top()
+
+
+'''
+o/p - 
+5 6 7 8 9 
+5 6 7 8 
+8
+'''
