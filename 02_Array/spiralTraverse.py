5e63dd6be9de224e3cf41e9da2181a6a112a88f8
Jyoti Jauhari <jyotijauhari222@gmail.com>
2021-10-01 12:45:51 +0530

    optimised code
    optimised code to work with this types of input as well ->  [[1,2,3,4],[5,6,7,8],[9,10,11,12]]

--- 02_Array/spiralTraverse.py
+++ 02_Array/spiralTraverse.py
@@ -12,11 +12,13 @@ def spiralTraverse(array):
 		for row in range(startRow + 1, endRow +1 ) :
 			result.append([row][endCol])
 
-		for col in reversed(range(startCol, endCol)):
-			result.append([endRow][col])
-
-		for row in reversed(range(startRow+1, endRow)):
-			result.append([row][startCol])
+		if startRow < endRow:
+			for col in reversed(range(startCol, endCol)):
+				result.append([endRow][col])
+	
+		if startCol < endCol:
+			for row in reversed(range(startRow+1, endRow)):
+				result.append([row][startCol])
 	
 		startRow += 1
 		startCol += 1
