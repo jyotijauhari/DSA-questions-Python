2a5e5069dea9008d31f19d2b77731d32cb4fb2fb
Jyoti Jauhari <jyotijauhari222@gmail.com>
2021-10-16 17:06:55 +0530

    Create 05_connected_components_dfs.py

---
+++ 12_Graph/graph_basics/05_connected_components_dfs.py
@@ -0,0 +1,31 @@
+def DFSRec(adj, s, visited):
+    visited[s] = True
+
+    print(s, end=' ')
+
+    for u in adj[s]:
+
+        if visited[u] == False:
+            DFSRec(adj, u, visited)
+
+
+def DFS(adj):
+    visited = [False] * len(adj)
+    res = 0
+    for u in range(len(adj)):
+        if visited[u] == False:
+            res+=1
+            DFSRec(adj, u, visited)
+            print()
+
+    return res
+
+
+def printGraph(adj):
+    for u, l in enumerate(adj):
+        print(u, l)
+
+
+adj = [[1, 2], [0, 2], [0, 1], [4], [3]]
+
+printGraph(adj)
