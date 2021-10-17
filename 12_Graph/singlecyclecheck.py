3fdf5c73ef9020542e1d6be9437ab0885a00ea2f
Jyoti Jauhari <jyotijauhari222@gmail.com>
2021-10-17 15:16:56 +0530

    other method added

--- 12_Graph/singlecyclecheck.py
+++ 12_Graph/singlecyclecheck.py
@@ -1,5 +1,33 @@
 #ip = [2,3,1,-4,-4,2]
 #op - true
+
+# O(N) time and O(1) space
+def hasSingleCycle(array):
+    # Write your code here.
+	i = 0
+    while array[i] != None:
+		jump = array[i]
+		array[i] = None
+		if jump >= 0:
+			i = (jump + i) % len(array)
+		else:
+			jump = abs(jump) % len(array)
+			if i - jump < 0:
+				i = len(array) + (i - jump)
+			else:
+				i -= jump
+	
+	if i != 0:
+		return False
+	print(array)
+	for x in array:
+		if x != None:
+			return False
+		
+	return True
+
+
+
 #t- O(N) and space = O(1)
 def hasSingleCycle(array):
 	numElementsVisited = 0
